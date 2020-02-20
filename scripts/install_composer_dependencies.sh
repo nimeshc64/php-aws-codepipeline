148ec4bc10a377845a0099130d405481677a58dd
Nimesh <nimeshc64@gmail.com>
2020-02-21 01:07:16 +0530

    Copy source files by  environment

--- scripts/install_composer_dependencies.sh
+++ scripts/install_composer_dependencies.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+# Copy source files to DEVELOPMENT environment
+if [ "$DEPLOYMENT_GROUP_NAME" == "development" ]; then
+sudo rsync -av /var/www/html/ /var/www/development/domain.com
+cd /var/www/development/domain.com && npm install
+fi
+# Copy source files to PRODUCTION environment
+if [ "$DEPLOYMENT_GROUP_NAME" == "production" ]; then
+sudo rsync -av /var/www/html/ /var/www/production/domain.com
+cd /var/www/production/domain.com && npm install
+fi
+
 cd /usr/share/nginx/html
 #yum install wget -y
 wget https://getcomposer.org/composer.phar
--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 echo "$DEPLOYMENT_GROUP_NAME"  >>  example.txt
+
+if [ "$DEPLOYMENT_GROUP_NAME" == "Staging" ]
+then
+    #do something
+fi
